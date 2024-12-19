616632943e1f83b69c0c25c5c06f90a48854c499
hekdk <desaidev034@gmail.com>
2024-12-19 12:21:39 +0530

    expense_tracker

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,13 +1,10 @@
+import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return },}
