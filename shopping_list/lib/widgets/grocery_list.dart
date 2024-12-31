9f20cc357186681b2df78f06461485cfff7896dd
hekdk <desaidev034@gmail.com>
2024-12-31 12:49:45 +0530

    shopping_list

---
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  @override
+  Widget build(BuildContext context) {
+    void _addItem() {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const NewItem(),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (ctx, index) => ListTile(
+          title: Text(groceryItems[index].name),
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: groceryItems[index].category.color,
+          ),
+          trailing: Text(groceryItems[index].quantity.toString()),
+        ),
+      ),
+    );
+  }
+}
