87491bf8c2ee59dd9d7bbc33e1fd5c89b9daf697
hekdk <desaidev034@gmail.com>
2025-01-09 10:37:11 +0530

    favorite_places

--- favorite_places/lib/widgets/location_input.dart
+++ favorite_places/lib/widgets/location_input.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -41,11 +44,16 @@ class _LocationInputState extends State<LocationInput> {
     });
 
     locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+    final url = Uri.parse(
+        'https://maps.gomaps.pro/maps/api/geocode/json?latlng=$lat,$lng&key=AlzaSyeqjfqgBr_Uax-e49MnPL_cOmXpEOOgYZZ');
+    final response = await http.get(url);
+    final resData = json.decode(response.body);
+    final data = resData['results'][0]['formatted_address'];
     setState(() {
       _isGettingLocation = false;
     });
-    print(locationData.latitude);
-    print(locationData.longitude);
   }
 
   @override
