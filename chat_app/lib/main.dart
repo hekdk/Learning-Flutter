ea30e8a184cced2122a6c5d3bcffcbf13919dd6a
hekdk <desaidev034@gmail.com>
2025-04-26 14:32:41 +0530

    chat_app

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(255, 63, 17, 177),
+        ),
+      ),
+      home: ,
+    );
+  }
+}
